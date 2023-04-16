a3e34f00e5166605d7c6118c383391bf2f3a827c
ImAHappyDuck <bosscj21@gcc.edu>
2023-04-16 18:02:35 -0400

    Minor stuff

--- src/Cipher.java
+++ src/Cipher.java
@@ -2,36 +2,31 @@ import java.util.Scanner;
 
 public abstract class Cipher {
 
+    public abstract char encrypt(char c);
 
-public abstract char encrypt (char c);
+    public abstract char decrypt(char c);
 
-public abstract char decrypt (char c);
-
-
-
-public String encrypt (String s)
-{
-    StringBuilder sb = new StringBuilder();
-    Scanner sc = new Scanner(s);
-    sc.useDelimiter("");
-    while (sc.hasNext())
-    {
-        sb.append(encrypt(sc.next()));
+    public String encrypt(String s) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(s);
+        sc.useDelimiter("");
+        while (sc.hasNext()) {
+            sb.append(encrypt(sc.next()));
+        }
+        return sb.toString();
     }
-    return sb.toString();
-}
-public String decrypt (String s)
-{
-    StringBuilder sb = new StringBuilder();
-    Scanner sc = new Scanner(s);
-    sc.useDelimiter("");
-    while (sc.hasNext())
-    {
-        sb.append(decrypt(sc.next()));
+
+    public String decrypt(String s) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(s);
+        sc.useDelimiter("");
+        while (sc.hasNext()) {
+            sb.append(decrypt(sc.next()));
+        }
+        return sb.toString();
     }
-    return sb.toString();
-}
-public abstract Cipher newCopy(Cipher c);//Should this be abstract???
+
+    public abstract Cipher newCopy(Cipher c);//Should this be abstract???
 
 
 }
