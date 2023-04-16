e4730cee5fff3842020b7bbd3717a9b9b45462ec
ImAHappyDuck <bosscj21@gcc.edu>
2023-04-16 17:46:20 -0400

    Made Cipher class and method bodies.

---
+++ src/Cipher.java
@@ -0,0 +1,30 @@
+public class Cipher {
+
+
+public char encrypt (char c)
+{
+
+}
+
+
+public char decrypt (char c)
+{
+
+}
+
+
+public String encrypt (String s)
+{
+
+}
+public String decrypt (String s)
+{
+
+}
+public Cipher newCopy(Cipher c)
+{
+
+}
+
+
+}
