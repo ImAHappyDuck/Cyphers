eb684955f769b33f7a296ec5281d0eb04c1b124c
ImAHappyDuck <bosscj21@gcc.edu>
2023-04-16 17:59:43 -0400

    Filled out methods in super class. Should now be completed.

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,30 +1,37 @@
-public class Cipher {
+import java.util.Scanner;
+
+public abstract class Cipher {
 
 
-public char encrypt (char c)
-{
+public abstract char encrypt (char c);
 
-}
+public abstract char decrypt (char c);
 
-
-public char decrypt (char c)
-{
-
-}
 
 
 public String encrypt (String s)
 {
-
+    StringBuilder sb = new StringBuilder();
+    Scanner sc = new Scanner(s);
+    sc.useDelimiter("");
+    while (sc.hasNext())
+    {
+        sb.append(encrypt(sc.next()));
+    }
+    return sb.toString();
 }
 public String decrypt (String s)
 {
-
+    StringBuilder sb = new StringBuilder();
+    Scanner sc = new Scanner(s);
+    sc.useDelimiter("");
+    while (sc.hasNext())
+    {
+        sb.append(decrypt(sc.next()));
+    }
+    return sb.toString();
 }
-public Cipher newCopy(Cipher c)
-{
-
-}
+public abstract Cipher newCopy(Cipher c);//Should this be abstract???
 
 
 }
