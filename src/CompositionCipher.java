0691f7680f0ce0ee33c830e80e217b662ecb5b95
ImAHappyDuck <bosscj21@gcc.edu>
2023-04-18 14:38:58 -0400

    Fixed bugs and wrote constructors in composition cipher. Passes all tests now.

--- src/CompositionCipher.java
+++ src/CompositionCipher.java
@@ -1,41 +1,63 @@
 import java.util.ArrayList;
-public class CompositionCipher extends Cipher{
+public class CompositionCipher extends Cipher {
 
     //Private array list of ciphers that are used to enrcypt and decrypt
     private ArrayList<Cipher> ciphers = new ArrayList<Cipher>();
 
-    //Two constructor classes, Should be all good
-    public CompositionCipher(){}
-    public CompositionCipher(CompositionCipher other){}
+    public ArrayList<Cipher> getCiphers() {
+        return ciphers;
+    }
 
-    //@Override
-    public char encrypt(char c){
-        //Encryption loops forward changing that value of nextChar
-        char nextChar = c;
-        for(int i = 0; i < ciphers.size(); i++){
-            nextChar = ciphers.get(i).encrypt(nextChar);
+    public void setCiphers(ArrayList<Cipher> ciphers) {
+        this.ciphers = ciphers;
+    }
+
+    //Two constructor classes, Should be all good
+    public CompositionCipher() {
+    }
+
+    public CompositionCipher(CompositionCipher other) {
+        ArrayList<Cipher> gB = new ArrayList<>();
+      for(Cipher c : other.getCiphers())
+      {
+          gB.add(c.newCopy());
+      }
+      this.ciphers = gB;
         }
-        return nextChar;
-    }
-    public char decrypt(char c){
-        //Decryption is the same but loops backwards
-        char nextChar = c;
-        for(int i = ciphers.size(); i>0; i--){
-            nextChar = ciphers.get(i).decrypt(nextChar);
+
+        //@Override
+        public char encrypt ( char c){
+            //Encryption loops forward changing that value of nextChar
+            char nextChar = c;
+            for (int i = 0; i < ciphers.size(); i++) {
+                nextChar = ciphers.get(i).encrypt(nextChar);
+            }
+            return nextChar;
         }
-        //stub
-        return nextChar;
+        public char decrypt ( char c){
+            //Decryption is the same but loops backwards
+            char nextChar = c;
+            for (int i = ciphers.size()-1; i > -1; i--) {
+                nextChar = ciphers.get(i).decrypt(nextChar);
+            }
+            //stub
+            return nextChar;
+        }
+
+        public Cipher newCopy () {
+            //Im not really sure if this is correct
+            CompositionCipher cc = new CompositionCipher(this);
+            return cc;
+            //stub
+            //return null;
+        }
+        public void add (Cipher theCipher){
+            //need to make a deep copy of the arguments
+            ciphers.add(theCipher);
+        }
     }
-    @Override
-    public Cipher newCopy() {
-        //Im not really sure if this is correct
-        CompositionCipher cc = new CompositionCipher();
-        return cc;
-        //stub
-        //return null;
-    }
-    public void add(Cipher theCipher){
-        //need to make a deep copy of the arguments
-        ciphers.add(theCipher);
-    }
-}
+
+
+
+
+
--- src/TestCiphers.java
+++ src/TestCiphers.java
@@ -77,16 +77,16 @@ public class TestCiphers {
 	}
 
 
-	//@Test
-//	public void compositionWithCaesar(){
-//		CompositionCipher comp_cc = new CompositionCipher();
-//		CaesarCipher temp_cc = new CaesarCipher(18);
-//		comp_cc.add(temp_cc);
-//
-//		String answer = "stuvwxyzabcdefghijklmnopqrSTUVDEFGPQR.$\t() {+}012389";
-//
-//		assertEquals("", runTest(comp_cc, answer, true));
-//	}
+	@Test
+	public void compositionWithCaesar(){
+		CompositionCipher comp_cc = new CompositionCipher();
+		CaesarCipher temp_cc = new CaesarCipher(18);
+		comp_cc.add(temp_cc);
+
+		String answer = "stuvwxyzabcdefghijklmnopqrSTUVDEFGPQR.$\t() {+}012389";
+
+		assertEquals("", runTest(comp_cc, answer, true));
+	}
 
 
 	@Test
@@ -110,119 +110,119 @@ public class TestCiphers {
 	}
 
 
-//	@Test
-//	public void compositionWithCaesar_StringMethods(){
-//		CompositionCipher comp_cc = new CompositionCipher();
-//		CaesarCipher temp_cc = new CaesarCipher(18);
-//		comp_cc.add(temp_cc);
-//
-//		String answer = "stuvwxyzabcdefghijklmnopqrSTUVDEFGPQR.$\t() {+}012389";
-//
-//		assertEquals("", runTest(comp_cc, answer, false));
-//	}
+	@Test
+	public void compositionWithCaesar_StringMethods(){
+		CompositionCipher comp_cc = new CompositionCipher();
+		CaesarCipher temp_cc = new CaesarCipher(18);
+		comp_cc.add(temp_cc);
+
+		String answer = "stuvwxyzabcdefghijklmnopqrSTUVDEFGPQR.$\t() {+}012389";
+
+		assertEquals("", runTest(comp_cc, answer, false));
+	}
 
 
-//	@Test
-//	public void compositionWithVigenere(){
-//		CompositionCipher comp_vc = new CompositionCipher();
-//		Integer[] shifts = {7, 6, 9};
-//		VigenereCipher temp_vc = new VigenereCipher( Arrays.asList(shifts) );
-//		comp_vc.add(temp_vc);
-//		String answer = "hhlkkonnrqquttxwwazzdccgffJIIMSSWVDHG.$\t() {+}012389";
-//
-//		// Since VigenereCipher is mutable (i.e., it changes which shift amount
-//		//   to use next each time a character is encrypted/decrypted),
-//		// we check for a deep copy by changing state with an "encrypt" call
-//		CompositionCipher copy = new CompositionCipher(comp_vc);
-//		copy.encrypt('x');
-//
-//		assertEquals("", runTest(comp_vc, answer, true));
-//	}
+	@Test
+	public void compositionWithVigenere(){
+		CompositionCipher comp_vc = new CompositionCipher();
+		Integer[] shifts = {7, 6, 9};
+		VigenereCipher temp_vc = new VigenereCipher( Arrays.asList(shifts) );
+		comp_vc.add(temp_vc);
+		String answer = "hhlkkonnrqquttxwwazzdccgffJIIMSSWVDHG.$\t() {+}012389";
 
-//	@Test
-//	public void compWithCompWithCaesar(){
-//		CompositionCipher comp_comp_cc = new CompositionCipher();
-//		CompositionCipher temp_comp = new CompositionCipher();
-//		CaesarCipher temp_cc = new CaesarCipher(4);
-//		String answer = "efghijklmnopqrstuvwxyzabcdEFGHPQRSBCD.$\t() {+}012389";
-//
-//		temp_comp.add(temp_cc);
-//		comp_comp_cc.add(temp_comp);
-//
-//		assertEquals("", runTest(comp_comp_cc, answer, true));
-//	}
-//
-//	@Test
-//	public void compWithTwoCaesars(){
-//		CompositionCipher comp_cc_cc = new CompositionCipher();
-//		CaesarCipher temp_cc1 = new CaesarCipher(19);
-//		CaesarCipher temp_cc2 = new CaesarCipher(12);
-//		String answer = "fghijklmnopqrstuvwxyzabcdeFGHIQRSTCDE.$\t() {+}012389";
-//
-//		comp_cc_cc.add(temp_cc1);
-//		comp_cc_cc.add(temp_cc2);
-//
-//		assertEquals("", runTest(comp_cc_cc, answer, true));
-//	}
-//
-//
-//	@Test
-//	public void compWithTwoVigeneres(){
-//		CompositionCipher comp_vc_vc = new CompositionCipher();
-//		{
-//			Integer[] v1 = {2, 6};
-//			Integer[] v2 = {12, 13, 14};
-//
-//			VigenereCipher temp_vc1 = new VigenereCipher(Arrays.asList(v1));
-//			VigenereCipher temp_vc2 = new VigenereCipher(Arrays.asList(v2));
-//
-//			comp_vc_vc.add(temp_vc1);
-//			comp_vc_vc.add(temp_vc2);
-//		}
-//		String answer = "ousvtzuaybzfagehflgmknlrmsQTRXZFDGMSN.$\t() {+}012389";
-//
-//		assertEquals("", runTest(comp_vc_vc, answer, true));
-//	}
-//
-//	@Test
-//	public void compWithCaesarAndVigenere(){
-//		CompositionCipher comp_vc_cc = new CompositionCipher();
-//		Integer[] v = {12, 13, 14};
-//
-//		VigenereCipher temp_vc = new VigenereCipher(Arrays.asList(v));
-//		CaesarCipher temp_cc = new CaesarCipher(5);
-//		comp_vc_cc.add(temp_vc);
-//		comp_vc_cc.add(temp_cc);
-//
-//		String answer = "rtvuwyxzbacedfhgikjlnmoqprTSUWCEGFPRQ.$\t() {+}012389";
-//
-//		assertEquals("", runTest(comp_vc_cc, answer, true));
-//	}
-//
-//	@Test
-//	public void compWithEverything(){
-//		CompositionCipher comp_mix = new CompositionCipher();
-//
-//		Integer[] v1 = {2, 6};
-//		Integer[] v2 = {12, 13, 14};
-//
-//		CompositionCipher temp_comp = new CompositionCipher();
-//		VigenereCipher temp_vc1 = new VigenereCipher(Arrays.asList(v1));
-//		VigenereCipher temp_vc2 = new VigenereCipher(Arrays.asList(v2));
-//		CaesarCipher temp_cc1 = new CaesarCipher(19);
-//		CaesarCipher temp_cc2 = new CaesarCipher(8);
-//
-//		temp_comp.add(temp_vc1);
-//		temp_comp.add(temp_cc1);
-//
-//		comp_mix.add(temp_cc2);
-//		comp_mix.add(temp_comp);
-//		comp_mix.add(temp_vc2);
-//
-//		String answer = "pvtwuavbzcagbhfigmhnlomsntRUSYAGEHNTO.$\t() {+}012389";
-//
-//		assertEquals("", runTest(comp_mix, answer, true));
-//	}
+		// Since VigenereCipher is mutable (i.e., it changes which shift amount
+		//   to use next each time a character is encrypted/decrypted),
+		// we check for a deep copy by changing state with an "encrypt" call
+		CompositionCipher copy = new CompositionCipher(comp_vc);
+		copy.encrypt('x');
+
+		assertEquals("", runTest(comp_vc, answer, true));
+	}
+
+	@Test
+	public void compWithCompWithCaesar(){
+		CompositionCipher comp_comp_cc = new CompositionCipher();
+		CompositionCipher temp_comp = new CompositionCipher();
+		CaesarCipher temp_cc = new CaesarCipher(4);
+		String answer = "efghijklmnopqrstuvwxyzabcdEFGHPQRSBCD.$\t() {+}012389";
+
+		temp_comp.add(temp_cc);
+		comp_comp_cc.add(temp_comp);
+
+		assertEquals("", runTest(comp_comp_cc, answer, true));
+	}
+
+	@Test
+	public void compWithTwoCaesars(){
+		CompositionCipher comp_cc_cc = new CompositionCipher();
+		CaesarCipher temp_cc1 = new CaesarCipher(19);
+		CaesarCipher temp_cc2 = new CaesarCipher(12);
+		String answer = "fghijklmnopqrstuvwxyzabcdeFGHIQRSTCDE.$\t() {+}012389";
+
+		comp_cc_cc.add(temp_cc1);
+		comp_cc_cc.add(temp_cc2);
+
+		assertEquals("", runTest(comp_cc_cc, answer, true));
+	}
+
+
+	@Test
+	public void compWithTwoVigeneres(){
+		CompositionCipher comp_vc_vc = new CompositionCipher();
+		{
+			Integer[] v1 = {2, 6};
+			Integer[] v2 = {12, 13, 14};
+
+			VigenereCipher temp_vc1 = new VigenereCipher(Arrays.asList(v1));
+			VigenereCipher temp_vc2 = new VigenereCipher(Arrays.asList(v2));
+
+			comp_vc_vc.add(temp_vc1);
+			comp_vc_vc.add(temp_vc2);
+		}
+		String answer = "ousvtzuaybzfagehflgmknlrmsQTRXZFDGMSN.$\t() {+}012389";
+
+		assertEquals("", runTest(comp_vc_vc, answer, true));
+	}
+
+	@Test
+	public void compWithCaesarAndVigenere(){
+		CompositionCipher comp_vc_cc = new CompositionCipher();
+		Integer[] v = {12, 13, 14};
+
+		VigenereCipher temp_vc = new VigenereCipher(Arrays.asList(v));
+		CaesarCipher temp_cc = new CaesarCipher(5);
+		comp_vc_cc.add(temp_vc);
+		comp_vc_cc.add(temp_cc);
+
+		String answer = "rtvuwyxzbacedfhgikjlnmoqprTSUWCEGFPRQ.$\t() {+}012389";
+
+		assertEquals("", runTest(comp_vc_cc, answer, true));
+	}
+
+	@Test
+	public void compWithEverything(){
+		CompositionCipher comp_mix = new CompositionCipher();
+
+		Integer[] v1 = {2, 6};
+		Integer[] v2 = {12, 13, 14};
+
+		CompositionCipher temp_comp = new CompositionCipher();
+		VigenereCipher temp_vc1 = new VigenereCipher(Arrays.asList(v1));
+		VigenereCipher temp_vc2 = new VigenereCipher(Arrays.asList(v2));
+		CaesarCipher temp_cc1 = new CaesarCipher(19);
+		CaesarCipher temp_cc2 = new CaesarCipher(8);
+
+		temp_comp.add(temp_vc1);
+		temp_comp.add(temp_cc1);
+
+		comp_mix.add(temp_cc2);
+		comp_mix.add(temp_comp);
+		comp_mix.add(temp_vc2);
+
+		String answer = "pvtwuavbzcagbhfigmhnlomsntRUSYAGEHNTO.$\t() {+}012389";
+
+		assertEquals("", runTest(comp_mix, answer, true));
+	}
 
 
 }
